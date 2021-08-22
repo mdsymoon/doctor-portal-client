76ae54bc6a55327af1f02c045bd583453db21533
mdsymoon <mdsymoon11@gmail.com>
2021-08-22 07:34:47 -0700

    navbar

---
+++ src/Components/Home/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderMain from '../HaederMain/HeaderMain';
+import Navbar from '../NavbarPage/NavbarPage';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <HeaderMain></HeaderMain>
+        </div>
+    );
+};
+
+export default Header;
