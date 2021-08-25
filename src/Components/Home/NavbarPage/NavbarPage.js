7ec693160333930387f0727bb0262dce567c7b09
mdsymoon <mdsymoon11@gmail.com>
2021-08-24 17:53:50 -0700

    navbar-fix

--- src/Components/Home/NavbarPage/NavbarPage.js
+++ src/Components/Home/NavbarPage/NavbarPage.js
@@ -1,41 +1,35 @@
-import React from 'react';
-
+import React from "react";
+import "./NavbarPage.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const NavbarPage = () => {
-    return (
-        <nav class="navbar navbar-expand-lg navbar-light ">
- 
-   
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link  ms-5   active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link  ms-5  " href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link  ms-5  " href="#">Dental Service</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link  ms-5 text-white  " href="#">Reviews</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link  ms-5 text-white" href="#">Blogs</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link  ms-5  text-white" href="#">Contact Us</a>
-        </li>
-        </ul>
-      
-      
-    
-  </div>
-</nav>
-    );
+  return (
+    <Navbar expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link href="#home" className="navbar-text">
+            Home
+          </Nav.Link>
+          <Nav.Link href="#link" className="navbar-text">
+            About
+          </Nav.Link>
+          <Nav.Link href="#link" className="navbar-text">
+            Dental Services
+          </Nav.Link>
+          <Nav.Link href="#link" className="navbar-text">
+            Reviews
+          </Nav.Link>
+          <Nav.Link href="#link" className="navbar-text">
+            Blog
+          </Nav.Link>
+          <Nav.Link href="#link" className="navbar-text">
+            Contact Us
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default NavbarPage;
