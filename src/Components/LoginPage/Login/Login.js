e7c4ad6f595bf2925681de369448641562227386
mdsymoon <mdsymoon11@gmail.com>
2021-08-25 21:52:12 -0700

    google-sign-in

--- src/Components/LoginPage/Login/Login.js
+++ src/Components/LoginPage/Login/Login.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import loginpageimg from '../../../images/Group 140.png';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
 
+firebase.initializeApp(firebaseConfig);
 
 const Login = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    const googleLogIn = () => {
+        firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+    }
+
     return (
        
              <div className="container">
@@ -22,7 +45,7 @@ const Login = () => {
               className="form-control mb-5"
               placeholder="Your Password"
             />
-            <button type="submit" className="info-button" >
+            <button type="submit" className="info-button" onClick={googleLogIn} >
               Google LogIn
             </button>
           </div>
